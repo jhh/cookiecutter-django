d428b08e5d6084338da886a6690d6ec1394470a8
Jeff Hutchison <jeff@jeffhutchison.com>
2022-10-08 08:54:54 -0400

    💥 Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cookiecutter Django
+
+Powered by [Cookiecutter](https://github.com/cookiecutter/cookiecutter), Cookiecutter Django is a opinionated template for starting a Django project.
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Cookiecutter template for Django projects.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ poetry pre-commit ];
+        };
+      }
+    );
+}

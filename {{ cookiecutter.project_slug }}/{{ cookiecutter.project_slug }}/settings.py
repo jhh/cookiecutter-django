a4ad1c44a41d35e92adbf8a28f4caa5606f8a306
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-17 17:59:27 -0500

    Add whitenoise for static files

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -63,6 +63,12 @@ TEMPLATES = [
 ]
 
 USE_TZ = True
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 STATIC_URL = "static/"
 
